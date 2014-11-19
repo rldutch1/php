5b19a095a7a22bb6b63669d0f685e9561797414c
Robert Holland <rob@robholland.com>
2014-11-18 23:04:51 -0700

    Added a comment to the top of the file.

--- passing_arguments_cli/md5hash.php
+++ passing_arguments_cli/md5hash.php
@@ -1,5 +1,5 @@
-
 <?php
+//Take one argument from the command line and md5 hash it. Use quotes on the command line if the argument has spaces in it.
 $str = $argv[1];
 $x = md5($str);
 echo $x . "\r\n";
