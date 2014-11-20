cbf3abae14b0d68609fe56b4edccf7faac8f3633
Robert Holland <rob@robholland.com>
2014-11-19 19:41:36 -0700

    Added filename variable.

--- passing_arguments_cli/md5hash.php
+++ passing_arguments_cli/md5hash.php
@@ -1,8 +1,10 @@
 <?php
+$filename = $argv[0];
 //Take one argument from the command line and md5 hash it. Use quotes on the command line if the argument has spaces in it.
 $str = $argv[1];
 $x = md5($str);
-echo $x . "\r\n";
+//echo $x . "\r\n";
+echo $filename . " says: \r\n";
 echo "update webusers set password = '" . $x . "' where username = 'rob';\r\n";
 
 
