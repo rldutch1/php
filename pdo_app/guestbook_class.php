c58baa2299e84200b07fcf15c236e22e1e5ac46d
Robert Holland <rob@robholland.com>
2015-04-12 11:41:16 -0700

    guestbook_class.php: Corrected the comment about the __construct() method.

--- pdo_app/guestbook_class.php
+++ pdo_app/guestbook_class.php
@@ -6,7 +6,7 @@ class GuestbookEntry {
 	$entry; //Entry isn't a field in the table. It will be constructed.
 
  	//Implement a constructor inside of the class. The constructor builds the $entry value.
- 	public function __construct() { //There is a double underscore in the --construct method.
+ 	public function __construct() { //There is a double underscore in the __construct method.
  		$this->entry = "{$this->name} posted: {$this->message}"; //The sentence is built here.
  		}
 
