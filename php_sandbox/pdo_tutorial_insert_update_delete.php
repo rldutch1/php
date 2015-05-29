e4ed577b1a81e586b4fcf390501892f18fce6c6e
Robert Holland <rob@robholland.com>
2015-05-28 23:40:28 -0700

    pdo_tutorial_insert_update_delete.php: Corrected the href link.

--- php_sandbox/pdo_tutorial_insert_update_delete.php
+++ php_sandbox/pdo_tutorial_insert_update_delete.php
@@ -1,5 +1,5 @@
 <?php
-echo "<a https://www.youtube.com/watch?v=7Rt-wXLud7o' target='_blank'>Lesson 8. Insert, update, and delete using Prepared Statements.</a>";
+echo "<a href='https://www.youtube.com/watch?v=7Rt-wXLud7o' target='_blank'>Lesson 8. Insert, update, and delete using Prepared Statements.</a>";
 
 include'pdo_tutorial_connect.php';
 
