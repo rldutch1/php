a3c8da7397530e6152b3ae2b22c3a963f9c44cf8
Robert Holland <rob@robholland.com>
2015-06-11 07:18:25 -0700

    Minor corrections to the files. Added a PHP form example from W3Schools.

--- php_sandbox/pdo_tutorial_queryA.php
+++ php_sandbox/pdo_tutorial_queryA.php
@@ -1,6 +1,6 @@
 <?php
 echo "<a href='https://www.youtube.com/watch?v=vqqUGNdRow8' target='_blank'>Lesson2 Youtube Video</a><br />";
-echo "<a href=''>Goto PDO Tutorial Lesson 3</a><br />";
+echo "<a href='pdo_tutorial_insert_update_delete.php'>Goto PDO Tutorial Lessons 3 - 8</a><br />";
 echo "<img src='lesson2_pdo_tutorial_pdo_tutorial_query.png'><br /><br />";
 //echo "<div><a href='" . include('pdo_tutorial_query.php') . "'>Run query</a><br /></div>";
 include('pdo_tutorial_query.php');
