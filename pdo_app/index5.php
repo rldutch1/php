f6eab11dfd98eaac6b84ea13e5e9bfa95b5f2da7
Robert Holland <rob@robholland.com>
2015-06-30 20:57:47 -0700

    pdo_app/index5.php: minor change.

--- pdo_app/index5.php
+++ pdo_app/index5.php
@@ -1,3 +1,30 @@
+<html>
+<head>
+<title></title>
+<script type="text/javascript" src="js/filename.js"></script>
+<link rel="stylesheet" type="text/css" href="css/filename.css" />
+
+<script type="text/javascript">
+//alert("Hello");
+</script>
+
+</head>
+<body>
+<p>
+$query = $handler->query('select * from guestbook');<br />
+if($query->rowCount()) {<br />
+		echo "Number of rows: ", $query->rowCount(),"<br />";
+		while($r = $query->fetch(PDO::FETCH_OBJ)){<br />
+			echo $r->name, " ", $r->message, " ", $r->posted, "<br />";
+		}<br />
+	} else {<br />
+		echo "No Results";<br />
+	}<br />
+</p>
+
+</body>
+</html>
+
 <?php
 echo "<br /><br /><a href='index.php'>Page 1</a><br />";
 include_once('connect_pdo.php');
@@ -14,7 +41,6 @@ if($query->rowCount()) {
 	} else {
 		echo "No Results";
 	}
-
-
-
 ?>
+
+
