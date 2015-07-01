fd9fcc3fbcdf562f04fe7f5487aad07cbf76e29e
Robert Holland <rob@robholland.com>
2015-07-01 08:30:18 -0700

    php_class_oop1.php: Up to 14:16 in the video.

--- php_sandbox/php_class_oop1.php
+++ php_sandbox/php_class_oop1.php
@@ -8,13 +8,39 @@
 //}
 //$variable1 = new User();
 
-class User {
-	public function __construct($message) { //This is a constructor method. There is a double underscroe.
-			echo 'Hi from ' . $message . '<br />';
+//class User {
+//	public function __construct($message) { //This is a constructor method. There is a double underscroe.
+//			echo 'Hi from ' . $message . '<br />';
+//		}
+//}
+//
+//$variable2 = new User('Rob');
+//$variable3 = new User('Stan');
+//$variable4 = new User('Tracy');
+
+class User { //
+
+	public $age; //assigning a value to age.
+	private $password;
+
+	public function __construct($age) {
+			$this->age = $age;
+			$this->password = 'asdfsdfd';$password;
+		}
+
+	public function getPassword($hint){
+			if ($hint == 'getit'){
+					return $this->password;
+				}
+			else {
+				return '<br />You don\'t have permission';
+				}
 		}
 }
 
-$variable2 = new User('Rob');
-$variable3 = new User('Stan');
-$variable4 = new User('Tracy');
+$rob = new User(46);
+echo $rob->age;
+echo $rob->getPassword('1getit');
+
+
 ?>
