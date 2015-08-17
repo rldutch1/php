a9ac213367040d712c30b602fc44ba92158a97b6
rldutch1 <rob@robholland.com>
2015-08-17 07:59:43 -0700

    readtextperline.php: Added some comments and organized the code a bit.

--- examples/readtextperline.php
+++ examples/readtextperline.php
@@ -1,14 +1,9 @@
 <?php
+//http://www.tizag.com/phpT/php-string-explode.php
 //read text per line and convert to array
 
 //for example, the input file is readtextperline.txt
 //the input file contains text below
-
-//one
-//two
-//three
-//four
-//five
 
 //++++++++++++++++++++++++++
 
@@ -23,10 +18,16 @@ for ($i=0;$i<count($convert);$i++)
     echo $convert[$i] . "\n"; //write value by index and add carriage return.
 }
 
+//The third argument doesn't seem to be working in this next example.
+$convert1 = explode("\n", $data,5); //Using the third (optional) argument which allows you to set the number of pieces explode can return. This means it will stop exploding once the number of pieces equals the set limit.
+echo "Line count: " . count($convert1) . "\n";
+
+for ($i=0;$i<count($convert1);$i++)
+{
+    echo $convert1[$i] . "\n"; //write value by index and add carriage return.
+}
+
 //++++++++++++++++++++++++++
 
-//Output :
-
-//one, two, three, four, five,
 ?>
 
