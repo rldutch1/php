7e5672681525f49e9f753df3cfd1baa26596ef28
Robert Holland <rob@robholland.com>
2016-06-27 23:26:11 -0700

    Adding String to Time (strtotime.php) and Start Date End Date (startdate_enddate.php).

---
+++ examples/strtotime.php
@@ -0,0 +1,11 @@
+<?php
+ 
+ // Set timezone
+ date_default_timezone_set('UTC');
+ 
+ echo strtotime("now") . "\n";
+ echo strtotime("10 October 2010") . "\n";
+ echo strtotime("next Friday") . "\n";
+ echo strtotime("last Tuesday"), "\n";
+ 
+?>
