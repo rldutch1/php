f941a6d041990cd129ca848c9306888e3caf63a9
rlholland <robert.holland@bannerhealth.com>
2016-06-30 00:04:02 -0700

    Modified md5hash.php to accept two arguments, password and username.

--- passing_arguments_cli/md5hash.php
+++ passing_arguments_cli/md5hash.php
@@ -1,11 +1,10 @@
 <?php
 $filename = $argv[0];
-//Take one argument from the command line and md5 hash it. Use quotes on the command line if the argument has spaces in it.
-$str = $argv[1];
-$x = md5($str);
-//echo $x . "\r\n";
+//Take two arguments from the command line and md5 hash the first one. Use quotes on the command line if the argument has spaces in it.
+$str = md5($argv[1]);
+$str1 = $argv[2];
 echo $filename . " says: \r\n";
-echo "update webusers set password = '" . $x . "' where username = 'rob';\r\n";
+echo "update webusers set password = '" . $str . "' where username = '".$str1."';\r\n";
 
 
 ?>
