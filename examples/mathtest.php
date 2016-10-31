d32659a571269a5a6944ad879770ac0d19c05e11
Robert Holland <rob@robholland.com>
2016-10-31 00:15:02 -0700

    Added more examples.

---
+++ examples/mathtest.php
@@ -0,0 +1,6 @@
+<?php
+$a = array(10,20,30,40,50,60,70,80,90,100,110,120,130,140,150,160,170,180,190,200);
+$b = count($a);
+$result = array_sum($a);
+echo "The average of $result divided by $b is " . $result / $b . ".\n";
+?>
