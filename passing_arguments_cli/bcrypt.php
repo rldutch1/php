2ab310a586a46131b241d12974aefff77e622a74
Robert Holland <rob@robholland.com>
2016-11-26 14:13:32 -0500

    Adding bcrypt for password encryption.

---
+++ passing_arguments_cli/bcrypt.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * We just want to hash our password using the current DEFAULT algorithm.
+ * This is presently BCRYPT, and will produce a 60 character result.
+ *
+ * Beware that DEFAULT may change over time, so you would want to prepare
+ * By allowing your storage to expand past 60 characters (255 would be good)
+ */
+
+//Source: http://php.net/manual/en/function.password-hash.php
+
+$filename = $argv[0];
+$username = $argv[1];
+$str = $argv[2];
+$password = password_hash($str, PASSWORD_DEFAULT);
+echo $password . "\r\n";
+echo "update webusers set password = '" . $password . "' where username = '" . $str . "';\r\n";
+
+?>
