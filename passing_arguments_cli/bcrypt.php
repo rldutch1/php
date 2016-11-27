66e193f3fbc1fcfd9e25ed6ba2c40a27f9058c15
Robert Holland <rob@robholland.com>
2016-11-26 23:57:19 -0700

    Adding a check if the username or password is empty.

--- passing_arguments_cli/bcrypt.php
+++ passing_arguments_cli/bcrypt.php
@@ -12,19 +12,23 @@
 $filename = $argv[0];
 $username = $argv[1];
 $password = $argv[2];
-//$hashedpass = password_hash($password, PASSWORD_DEFAULT);
-$hashedpass = password_hash($password, PASSWORD_BCRYPT); //The password_hash function returns a string that contains both the hash and the salt.
-echo "The username is: " . $username . " and password is: " . $hashedpass . "\r\n";
-echo "\r\n";
-echo "update webusers set password = '" . $hashedpass . "' where username = '" . $username . "';\r\n";
-echo "\r\n";
+
+    if (!empty($username) && !empty($password)){
+    $hashedpass = password_hash($password, PASSWORD_BCRYPT); //The password_hash function returns a string that contains both the hash and the salt.
+    echo "The username is: " . $username . " and password is: " . $hashedpass . "\r\n";
+    echo "\r\n";
+    echo "update webusers set password = '" . $hashedpass . "' where username = '" . $username . "';\r\n";
+    echo "\r\n";
 
 
-if (password_verify($password, $hashedpass)){
-	echo "Password verified, access granted!\r\n";
-}
-else{
-	echo "Password not verified, access denied!\r\n";
-}
-
+    	if (password_verify($password, $hashedpass)){
+    	echo "Password verified, access granted!\r\n";
+    	}
+    	else{
+    		echo "Password not verified, access denied!\r\n";
+    	}
+		}
+		else {
+			echo "Username and/or password are blank. Please try again.\r\n";
+		}
 ?>
