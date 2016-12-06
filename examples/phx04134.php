3a267eb405a87da472c321986ed26240dda18857
Robert Holland <Robert.Holland@bannerhealth.com>
2016-12-06 11:14:10 -0700

    Modified phx0134.php, replaced the column names with the column number.

--- examples/phx04134.php
+++ examples/phx04134.php
@@ -16,10 +16,29 @@ $handler = new PDO("sqlsrv:server=$serverName; Database=Canned_mPage", "mpage", 
 
 //Print the contents of the table.
 	$sql = 'select * from mpage_settings';
-	foreach ($handler -> query($sql) as $row) {
-		print $row['Topic Name'] . "\t";
-		print $row['Filter Name'] . "\t";
-		print $row['Saved Value'] . "\t";
+	foreach ($handler -> query($sql) as $col) {
+		print $col[0] . "\t";
+		print $col[1] . "\t";
+		print $col[2] . "\t";
+		print $col[3] . "\t";
+		print $col[4] . "\t";
+		print $col[5] . "\t";
+		print $col[6] . "\t";
+		print $col[7] . "\t";
+		print $col[8] . "\t";
+		print $col[9] . "\t";
+		print $col[10] . "\t";
+		print $col[11] . "\t";
+		print $col[12] . "\t";
+		print $col[13] . "\t";
+		print $col[14] . "\t";
+		print $col[15] . "\t";
+		print $col[16] . "\t";
+		print $col[17] . "\t";
+		print $col[18] . "\t";
+		print $col[19] . "\t";
+		print $col[20] . "\t";
+		print $col[21] . "\t";
 	}
 
 ?>
