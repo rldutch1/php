b017bdc3c9421ea5988c5a1d4ddbb836468ff394
Robert Holland <Robert.Holland@bannerhealth.com>
2016-12-06 10:26:54 -0700

    Renamed  to .

--- examples/phx04134.php
+++ examples/phx04134.php
@@ -4,9 +4,9 @@ $serverName = "phx04134.bhs.bannerhealth.com"; //Servername/Instancename
 
 //Since UID and password are not specified the connection will attempt using the Windows authentication.
 
-$conn = new PDO("sqlsrv:server=$serverName; Database=Canned_mPage", "mpage", "mpage");
+$handler = new PDO("sqlsrv:server=$serverName; Database=Canned_mPage", "mpage", "mpage");
 
-	if($conn) {
+	if($handler) {
 		echo "Connection established.<br />";
 	}
 	else {
@@ -16,7 +16,7 @@ $conn = new PDO("sqlsrv:server=$serverName; Database=Canned_mPage", "mpage", "mp
 
 //Print the contents of the table.
 	$sql = 'select * from mpage_settings';
-	foreach ($conn -> query($sql) as $row) {
+	foreach ($handler -> query($sql) as $row) {
 		print $row['Topic Name'] . "\t";
 		print $row['Filter Name'] . "\t";
 		print $row['Saved Value'] . "\t";
