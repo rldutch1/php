0757632150aef916ce3a2ff90d44402766cfce8d
Robert Holland <rob@robholland.com>
2017-09-07 13:19:56 -0700

    Added more links and information for creating custom login pages using Wordpress authentication.

--- wp_custom_login_rh/wordpress_authentication_on_non_wordpress_page.php
+++ wp_custom_login_rh/wordpress_authentication_on_non_wordpress_page.php
@@ -10,4 +10,49 @@ header('HTTP/1.1 200 OK');
 //Note that the file must be in the same folder as your theme.
 
 //Then you can use is_user_logged_in before executing the rest of the script. If use is not logged in, then just trigger the login form or a login plugin
+
+?>
+
+/*
+https://stackoverflow.com/questions/29966695/wordpress-how-to-use-authentication-for-other-code
+You only need to require 'wp-load.php' in your seperate PHP file. wp-load.php is in the root of your WordPress installation. You should modify the path if the script is somewhere else. Like this,
+*/
+<?php
+
+    require_once 'wp-load.php';
+
+    if( is_user_logged_in() ) {
+        // true
+    } else {
+        // false
+    }
+
+?>
+
+<?php
+//https://stackoverflow.com/questions/2810124/how-to-add-a-php-page-to-wordpress/39800534#39800534
+?>
+
+<?php
+//https://stackoverflow.com/questions/22748947/how-to-authenticate-a-wordpress-user-in-external-script
+/*
+For anyone else who finds this I simply had to add some global variables as well as passed a string username into wp_authenticate instead of the user id and finally included wp-blog-header.php instead of wp-load.php. Here is my final code:
+*/
+function authentication ($user, $pass){
+  global $wp, $wp_rewrite, $wp_the_query, $wp_query;
+
+  if(empty($user) || empty($pass)){
+    return false;
+  } else {
+    require_once('/home/USERNAME/public_html/DOMAIN/wp-blog-header.php');
+    $status = false;
+    $auth = wp_authenticate($user, $pass );
+    if( is_wp_error($auth) ) {
+      $status = false;
+    } else {
+      $status = true;
+    }
+    return $status;
+  }
+}
 ?>
