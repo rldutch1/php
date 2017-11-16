5f49598f35a7d6af04bb4ab3385b9fb6fca3e2c2
Robert Holland <Robert.Holland@bannerhealth.com>
2017-11-15 22:57:04 -0700

    Timezone_identifiers_list();

---
+++ php_sandbox/timezone_identifiers_list.php
@@ -0,0 +1,13 @@
+<?php
+//Program Author: Robert Holland
+//Program Name: test.php
+//Creation Date: Wednesday, November 15, 2017 10:53:07 AM
+//Last Modified:
+//Purpose:
+//Request #:
+//----------------------------------------------------------------------------
+
+//echo json_encode(timezone_identifiers_list());
+
+print_r(timezone_identifiers_list());
+?>
