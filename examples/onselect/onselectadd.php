99aa4e2bce6080f215b95758a24278296e1a0f12
Robert Holland <rob@robholland.com>
2018-04-23 02:03:33 -0700

    Changed the element id's so they will be in numerical order.

--- examples/onselect/onselectadd.php
+++ examples/onselect/onselectadd.php
@@ -21,62 +21,61 @@
 			<h2>Add More Names</h2>
 		</div>
 			<ul >
-    <li id="li_3" >
-    <label class="description" for="element_3">Name </label>
-    <span>
-      <input id="element_3_1" name= "element_3_1" class="element text" maxlength="255" size="8" value=""/>
-      <label>First</label>
-    </span>
-    <span>
-      <input id="element_3_2" name= "element_3_2" class="element text" maxlength="255" size="14" value=""/>
-      <label>Last</label>
-    </span><p class="guidelines" id="guide_3"><small>Name instructions.</small></p>
-    </li>
-    <li id="li_9" >
-    <label class="description" for="element_9">Birthdate </label>
-    <span>
-      <input id="element_9_1" name="element_9_1" class="element text" size="2" maxlength="2" value="" type="text"> /
-      <label for="element_9_1">MM</label>
-    </span>
-    <span>
-      <input id="element_9_2" name="element_9_2" class="element text" size="2" maxlength="2" value="" type="text"> /
-      <label for="element_9_2">DD</label>
-    </span>
-    <span>
-      <input id="element_9_3" name="element_9_3" class="element text" size="4" maxlength="4" value="" type="text">
-      <label for="element_9_3">YYYY</label>
-    </span>
+				<li id="li_1" >
+				<label class="description" for="element_1">Name </label>
+				<span>
+				  <input id="element_1_1" name= "element_1_1" class="element text" maxlength="255" size="20" value=""/>
+				  <label>First</label>
+				</span>
+				<span>
+				  <input id="element_1_2" name= "element_1_2" class="element text" maxlength="255" size="20" value=""/>
+				  <label>Last</label>
+				</span><p class="guidelines" id="guide_1"><small>Name instructions.</small></p>
+				</li>
+				<li id="li_2" >
+				<label class="description" for="element_2">Birthdate </label>
+				<span>
+				  <input id="element_2_1" name="element_2_1" class="element text" size="2" maxlength="2" value="" type="text"> /
+				  <label for="element_2_1">MM</label>
+				</span>
+				<span>
+				  <input id="element_2_2" name="element_2_2" class="element text" size="2" maxlength="2" value="" type="text"> /
+				  <label for="element_2_2">DD</label>
+				</span>
+				<span>
+				  <input id="element_2_3" name="element_2_3" class="element text" size="4" maxlength="4" value="" type="text">
+				  <label for="element_2_3">YYYY</label>
+				</span>
 
-    <span id="calendar_9">
-      <img id="cal_img_9" class="datepicker" src="calendar.gif" alt="Pick a date.">
-    </span>
-    <script type="text/javascript">
-      Calendar.setup({
-      inputField	 : "element_9_3",
-      baseField    : "element_9",
-      displayArea  : "calendar_9",
-      button		 : "cal_img_9",
-      ifFormat	 : "%B %e, %Y",
-      onSelect	 : selectDate
-      });
-    </script>
-    <p class="guidelines" id="guide_9"><small>Date instructions.</small></p>
-    </li>
-    <li id="li_1" >
-    <label class="description" for="element_1">Hometown</label>
-    <div>
-      <input id="element_1" name="element_1" class="element text medium" type="text" maxlength="255" value=""/>
-    </div><p class="guidelines" id="guide_1"><small>Hometown instructions.</small></p>
-    </li>
-    <li id="li_1" >
-    <label class="description" for="element_1">Job</label>
-    <div>
-      <input id="element_1" name="element_1" class="element text medium" type="text" maxlength="255" value=""/>
-    </div><p class="guidelines" id="guide_1"><small>Job instructions.</small></p>
-    </li>
+				<span id="calendar_2">
+				  <img id="cal_img_2" class="datepicker" src="calendar.gif" alt="Pick a date.">
+				</span>
+				<script type="text/javascript">
+				  Calendar.setup({
+				  inputField	 : "element_2_3",
+				  baseField    : "element_2",
+				  displayArea  : "calendar_2",
+				  button		 : "cal_img_2",
+				  ifFormat	 : "%B %e, %Y",
+				  onSelect	 : selectDate
+				  });
+				</script>
+				<p class="guidelines" id="guide_2"><small>Date instructions.</small></p>
+				</li>
+				<li id="li_3" >
+				<label class="description" for="element_3">Hometown</label>
+				<div>
+				  <input id="element_3" name="element_3" class="element text medium" type="text" maxlength="255" value=""/>
+				</div><p class="guidelines" id="guide_3"><small>Hometown instructions.</small></p>
+				</li>
+				<li id="li_4" >
+				<label class="description" for="element_4">Job</label>
+				<div>
+				  <input id="element_4" name="element_4" class="element text medium" type="text" maxlength="255" value=""/>
+				</div><p class="guidelines" id="guide_4"><small>Job instructions.</small></p>
+				</li>
     <li class="buttons">
     <input type="hidden" name="form_id" value="71303" />
-
     <input id="saveForm" class="button_text" type="submit" name="submit" value="Submit" />
     </li>
   </ul>
