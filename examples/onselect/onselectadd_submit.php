135fc36b6a8c6a1b15bdb91a16ff52753d9ffb9c
Robert Holland <rob@robholland.com>
2018-04-23 01:46:29 -0700

    Adding necessary appnitro files to display the form correctly.

---
+++ examples/onselect/onselectadd_submit.php
@@ -0,0 +1,8 @@
+<?php
+
+echo "<html><head><title>MySQLi</title></head><body>";
+echo "<h1>Submit button doesn't do anything just yet.</h1>";
+echo "<br /><a href='onselectadd.php'>Go Back</a>";
+echo "</body></html>";
+
+?>
---
+++ examples/onselect/onselectadd_submit_pdo.php
@@ -0,0 +1,8 @@
+<?php
+
+echo "<html><head><title>MySQL PDO</title></head><body>";
+echo "<h1>Submit button doesn't do anything just yet.</h1>";
+echo "<br /><a href='onselectadd_pdo.php'>Go Back</a>";
+echo "</body></html>";
+
+?>
