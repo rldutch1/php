781ace54fec77bcc188202899b9e188f9d440231
Robert Holland <Robert.Holland@bannerhealth.com>
2018-08-25 14:17:58 -0700

    Corrected the calculation for the timezone so the user only has to enter the + or - and the hour number for the timezone.

--- examples/if_else_using_time.php
+++ examples/if_else_using_time.php
@@ -1,24 +1,26 @@
 <?php
 //session_start();
 //session_regenerate_id(true);
-$h = '7';// Hour for time zone goes here e.g. +7 or -4, just remove the + or -
+$h = '+3';// Hour for time zone goes here e.g. +7 or -4 etc.
 $hm = $h * 60; //minutes
 $ms = $hm * 60; //milliseconds
 //$gmdate = gmdate('Y-m-d H:i:s', time()-($ms)); // the '-' (Minus sign) can be switched to a plus if that's what your time zone is.
 //echo '<br />Your current time now is: ' . $gmdate . '.<br />' ;
-//echo 'Your current hour is: ' . $gmdate = gmdate('H', time()-($ms));
-$gmdate = gmdate('H', time()-($ms));
+//echo 'Your current hour is: ' . $gmdate = gmdate('H', time()+($ms));
+
+
+$gmdate = gmdate('H', time()+($ms));
 
 if ($gmdate<"12")
   {
-  echo "The hour is: " . $gmdate . " Have a good morning!";
+  echo "The hour is: " . $gmdate . ":00 in time zone {$h}. Have a good morning!";
   }
 elseif ($gmdate<"18")
   {
-  echo "The hour is: " . $gmdate . " Have a good afternoon!";
+  echo "The hour is: " . $gmdate . ":00 in time zone {$h}. Have a good afternoon!";
   }
 else
   {
-  echo "The hour is: " . $gmdate . " Have a good night!";
+  echo "The hour is: " . $gmdate . ":00 in time zone {$h}. Have a good night!";
   }
 ?>
