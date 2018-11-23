fa06a923f6e111e127cd640a420fb084329972df
Robert Holland <Robert.Holland@bannerhealth.com>
2018-11-23 14:02:04 -0700

    Added note about where I got the Access DB query from.

--- examples/access_db/cidb.mdb.php
+++ examples/access_db/cidb.mdb.php
@@ -17,12 +17,13 @@ $filename="cidb.output.txt";
 try {
    $handler = new PDO("odbc:Driver={Microsoft Access Driver (*.mdb, *.accdb)};Dbq=//phx00027/unix/CSM/Database Management/CIDB.MDB;");
 
-   $static_content_url=$handler->query("SELECT DISTINCT(EUC_EnvPkg.PkgNum) AS Package_Num, EUC_EnvPkg.pkgversion AS Version, EUC_Env.EnvName AS Domain, EUC_EnvPkg.InstallDate, EUC_PkgDefinition.PkgName, EUC_PkgType.PkgTypeDescription
+//I cheated by setting up the query in MSAcess then copying and pasting it here.
+   $query=$handler->query("SELECT DISTINCT(EUC_EnvPkg.PkgNum) AS Package_Num, EUC_EnvPkg.pkgversion AS Version, EUC_Env.EnvName AS Domain, EUC_EnvPkg.InstallDate, EUC_PkgDefinition.PkgName, EUC_PkgType.PkgTypeDescription
 FROM (EUC_EnvPkg INNER JOIN EUC_Env ON EUC_EnvPkg.Env_id = EUC_Env.Env_id) INNER JOIN (EUC_PkgDefinition INNER JOIN EUC_PkgType ON EUC_PkgDefinition.PkgTypeCd = EUC_PkgType.PkgTypeCd) ON EUC_EnvPkg.PkgNum = EUC_PkgDefinition.PkgNum
 WHERE (((EUC_EnvPkg.PkgNum) in (". $argument1 ."))) order by EUC_EnvPkg.PkgNum, EUC_Env.EnvName;");
 $dataout = fopen($filename, "w") or die("Unable to open file!");
 $txt = "";
-	foreach($static_content_url as $r){
+	foreach($query as $r){
 		//echo "Package Number: " . $r[0] . "\r\nPackage Version: " . $r[1] . "\r\nDomain: " . $r[2] . "\r\nInstall Date: " . $r[3] . "\r\nPackage Name: " . $r[4] . "\r\nPackage Type: " . $r[5] . "\r\n\r\n";
 		//file_put_contents($filename, "Package Number: " . $r[0] . "\r\nPackage Version: " . $r[1] . "\r\nDomain: " . $r[2] . "\r\nInstall Date: " . $r[3] . "\r\nPackage Name: " . $r[4] . "\r\nPackage Type: " . $r[5] . "\r\n\r\n",0);
 		$txt .="Package Number: " . $r[0] . "\r\nPackage Version: " . $r[1] . "\r\nDomain: " . $r[2] . "\r\nInstall Date: " . $r[3] . "\r\nPackage Name: " . $r[4] . "\r\nPackage Type: " . $r[5] . "\r\n\r\n";
@@ -30,9 +31,9 @@ $txt = "";
 		fwrite($dataout, $txt);
 
 //Show all package numbers begin:
-//  $static_content_url=$handler->query("SELECT DISTINCT(EUC_EnvPkg.PkgNum) AS Package_Num, EUC_EnvPkg.pkgversion AS Version FROM EUC_EnvPkg;");
+//  $query=$handler->query("SELECT DISTINCT(EUC_EnvPkg.PkgNum) AS Package_Num, EUC_EnvPkg.pkgversion AS Version FROM EUC_EnvPkg;");
 
-//	foreach($static_content_url as $r){
+//	foreach($query as $r){
 //		echo "Package Number: " . $r[0] . "\r\nPackage Version: " . $r[1] . "\r\n\r\n";
 //	}
 //Show all package number end:
