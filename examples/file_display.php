fbc8384e20c26d829eea02b6114df30681314524
Robert Holland <rob@robholland.com>
2020-05-16 10:02:12 -0700

    Modified file_display.php.

--- examples/file_display.php
+++ examples/file_display.php
@@ -1,5 +1,11 @@
 <?php
 
-
+//Display the current filename:
  echo "This filename is: " . basename(__FILE__) . "<br />\r\n";
+
+//Display all files in a directory using "glob":
+	foreach(glob('*.*') as $file) {
+		echo "<a href='" . $file . "'>" . $file . "</a>\r\n";
+	}
 ?>
+
