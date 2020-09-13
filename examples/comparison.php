b996810fd1c4ea25cf768932289534f70470755e
Robert Holland <rob@robholland.com>
2020-09-13 07:35:17 -0700

    Adding PHP comparison and default parameters example.

---
+++ examples/comparison.php
@@ -0,0 +1,17 @@
+<?php
+//https://stackoverflow.com/questions/8236354/php-is-null-or-empty
+echo "\r\nPHP Comparison:\r\n
+The == vs ===\r\n
+== will check the value of the variable\r\n
+=== will check the type of the variable as well as the value\r\n
+\r\n
+'' == NULL would return true\r\n
+0 == NULL would return true\r\n
+false == null would return true\r\n
+where as\r\n
+'' === NULL would return false\r\n
+0 === NULL would return false\r\n
+false === NULL would return false\r\n
+";
+
+?>
---
+++ examples/default_parameters.php
@@ -0,0 +1,42 @@
+<?php
+echo "Hello World!\r\n<br>";
+
+$a = 0;
+// True because $a is set
+if (isset($a)) {
+  echo "Variable 'a' is set.\r\n<br>";
+}
+
+$b = null;
+// False because $b is NULL
+if (isset($b)) {
+  echo "Variable 'b' is set.\r\n<br>";
+}
+else if (is_null($b)){
+	echo "Variable 'b' is not set because it is null.\r\n<br>";
+}
+
+$firstname = null;
+$lastname  = null;
+
+function hello($firstname, $lastname){
+if (isset($firstname) && isset($lastname)) {
+echo "Hello {$firstname} {$lastname}\r\n<br>";
+}
+else if (!isset($lastname)){ //If second argument is not set.
+echo "Hello {$firstname}.\r\n<br>";//Only echo the first argument.
+}
+}
+
+function hello1($firstname){
+// True because $a and $b are set
+if (isset($firstname)) {
+  echo "Hello {$firstname}.\r\n<br>";
+}
+}
+
+hello1("Robert-hello1");
+hello("Robert", "Holland-hello");
+hello("Aiko", null); //The second argument must be present even if it is empty.
+
+?>
