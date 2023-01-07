bed9e148fb1327791b2a5e6687cfa0e3a994b62f
Robert Holland <rob@robholland.com>
2023-01-07 09:15:22 -0700

    preg_match.php update.

--- examples/preg_match.php
+++ examples/preg_match.php
@@ -14,23 +14,23 @@ echo nl2br(htmlentities($data));
             $formdata[&#039;database_username&#039;] = trim($_POST[&quot;database_username&quot;]);<br />
         }<br />
 <br />
-	if (!preg_match(&quot;/^[a-zA-Z0-9-_. ]+$/&quot;, $_POST[&quot;database_username&quot;])) //Allow letters, numbers, underscore, dash, and periods. No spaces.<br />
+	if (!preg_match(&quot;/^[a-zA-Z0-9-_. ]+$/&quot;, $_POST[&quot;database_password&quot;])) //Allow letters, numbers, underscore, dash, and periods. No spaces.<br />
         {<br />
-            $error .= &#039;&lt;li&gt;Only letters, numbers, underscore, dash, and periods in username allowed&lt;/li&gt;&#039;;<br />
+            $error .= &#039;&lt;li&gt;Only letters, numbers, underscore, dash, and periods in password allowed&lt;/li&gt;&#039;;<br />
         }<br />
         else<br />
         {<br />
-            $formdata[&#039;database_username&#039;] = trim($_POST[&quot;database_username&quot;]);<br />
+            $formdata[&#039;database_password&#039;] = trim($_POST[&quot;database_password&quot;]);<br />
         }<br />
 <br />
 <br />
-	if (!preg_match(&quot;/^[\w\s\.-]*$/&quot;, $_POST[&quot;database_username&quot;])) //Allow anything including empty string (useless except for testing).<br />
+	if (!preg_match(&quot;/^[\w\s\.-]*$/&quot;, $_POST[&quot;database_password&quot;])) //Allow anything including empty string (useless except for testing).<br />
         {<br />
             $error .= &#039;&lt;li&gt;You should never see this because anything is allowed.&lt;/li&gt;&#039;;<br />
         }<br />
         else<br />
         {<br />
-            $formdata[&#039;database_username&#039;] = trim($_POST[&quot;database_username&quot;]);<br />
+            $formdata[&#039;database_password&#039;] = trim($_POST[&quot;database_password&quot;]);<br />
         }<br />
 <?php 
 $data="?>";
