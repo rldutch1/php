d030371bd6e27248ff9246c0ad75f720a9a33375
Robert Holland <rob@robholland.com>
2023-03-09 08:15:45 -0700

    Moved the comment above the work.

--- examples/mariadb_grant.php
+++ examples/mariadb_grant.php
@@ -48,6 +48,11 @@ function myfunction($value,$key)
  //Example:
  //GRANT USAGE ON robdba3.* TO 'robdba5'@'%' REQUIRE NONE WITH MAX_QUERIES_PER_HOUR 0 MAX_CONNECTIONS_PER_HOUR 0 MAX_UPDATES_PER_HOUR 0 MAX_USER_CONNECTIONS 0; GRANT ALL PRIVILEGES ON `robdba3`.* TO 'robdba5'@'%'; GRANT ALL PRIVILEGES ON `robdba3\_%`.* TO 'robdba5'@'%';
  }
+
+//Remove these critical system databases from the output.
+ //information_schema
+ //mysql
+ //performance_schema
  $i=count($rows);
  for($x = 0; $x < $i; $x++){
  	$y=$rows[$x]; //Reduce the array.
@@ -56,10 +61,5 @@ function myfunction($value,$key)
  	$y = array_diff($y, array("information_schema", "performance_schema","mysql"));
  array_walk($y,"myfunction");
  }
-
-//Remove these critical system databases from the output.
- //information_schema
- //mysql
- //performance_schema
 
 ?>
