e272daf892b9006b6c826b7d1a9ce0cbc6d769f6
Robert Holland <rob@robholland.com>
2023-12-30 23:13:34 -0700

    Updated mariadb_grant.php

--- examples/mariadb_grant.php
+++ examples/mariadb_grant.php
@@ -1,5 +1,3 @@
-<?php
-echo "
 &lt;?php<br />
 // ----------------------------------------------------------------------------<br />
 // Script Author: Robert Holland<br />
@@ -10,8 +8,7 @@ echo "
 // Purpose: Generate MariaDB GRANT statements for databases (This script prevents system databases from being modified).<br />
 // ----------------------------------------------------------------------------<br />
 <br />
-//Information sources:
-//How to write prepare and execute statements in OOP PDO?<br />
+//Information sources: //How to write prepare and execute statements in OOP PDO?<br />
 //https://stackoverflow.com/questions/42551050/how-to-write-prepare-and-execute-statements-in-oop-pdo<br />
 //https://stackoverflow.com/questions/18679448/pdo-class-is-this-technically-correct<br />
 //https://stackoverflow.com/questions/369602/deleting-an-element-from-an-array-in-php<br />
@@ -20,54 +17,54 @@ echo "
 <br />
 include&#039;connect.php&#039;;<br />
 <br />
-\$dbshow = new Connection(); //Instantiate a new connection.<br />
-\$dbshow-&gt;myQuery(&quot;show databases;&quot;);<br />
-\$rows = \$dbshow-&gt;All();<br />
+$dbshow = new Connection(); //Instantiate a new connection.<br />
+$dbshow-&gt;myQuery(&quot;show databases;&quot;);<br />
+$rows = $dbshow-&gt;All();<br />
 <br />
 //Generate random string of specified length.<br />
-  function generateRandomString(\$length) {<br />
-    \$characters = &#039;0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ&#039;;<br />
-    //\$characters = &#039;abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ&#039;; //Random uppercase and lowercase letters only.<br />
-    //\$characters = &#039;abcdefghijklmnopqrstuvwxyz&#039;; //Random lowercase letters only.<br />
-    //\$characters = &#039;ABCDEFGHIJKLMNOPQRSTUVWXYZ&#039;; //Random uppercase letters only.<br />
-    //\$characters = &#039;0123456789&#039;; //Random numbers only.<br />
-    \$charactersLength = strlen(\$characters);<br />
-    \$randomString = &#039;&#039;;<br />
-  for (\$i = 0; \$i &lt; \$length; \$i++) {<br />
-    \$randomString .= \$characters[rand(0, \$charactersLength - 1)];<br />
-  }<br />
-    return \$randomString;<br />
-  }<br />
-//\$ThePassword = generateRandomString(64); //Pass the number of random characters to generate.<br />
-//echo \$ThePassword . &quot;\\r\\n&quot;;<br />
+function generateRandomString($length) {<br />
+$characters = &#039;0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ&#039;;<br />
+//$characters = &#039;abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ&#039;; //Random uppercase and lowercase letters only.<br />
+//$characters = &#039;abcdefghijklmnopqrstuvwxyz&#039;; //Random lowercase letters only.<br />
+//$characters = &#039;ABCDEFGHIJKLMNOPQRSTUVWXYZ&#039;; //Random uppercase letters only.<br />
+//$characters = &#039;0123456789&#039;; //Random numbers only.<br />
+$charactersLength = strlen($characters);<br />
+$randomString = &#039;&#039;;<br />
+for ($i = 0; $i &lt; $length; $i++) {<br />
+$randomString .= $characters[rand(0, $charactersLength - 1)];<br />
+}<br />
+return $randomString;<br />
+}<br />
+//$ThePassword = generateRandomString(64); //Pass the number of random characters to generate.<br />
+//echo $ThePassword . &quot;\r\n&quot;;<br />
+echo &quot;See notes.robholland.net for more grant information.&lt;br /&gt;&lt;br &gt;&quot;;<br />
 <br />
-function myfunction(\$value,\$key)<br />
- {<br />
- 	\$dbusername = &quot;XXXXX&quot;;<br />
- 	\$ThePassword = generateRandomString(8); //Random 8 characters.<br />
- //echo &quot;The key \$key has the value \$value. &quot;;<br />
- echo &quot;CREATE USER &#039;\$dbusername&#039;@&#039;%&#039; IDENTIFIED VIA mysql_native_password USING &#039;\$ThePassword&#039;; \\r\\n&quot;;<br />
- echo &quot;CREATE USER &#039;\$dbusername&#039;@&#039;%&#039; SET PASSWORD FOR &#039;\$dbusername&#039;@&#039;%&#039; = PASSWORD(&#039;\$ThePassword&#039;); \\r\\n&quot;;<br />
- echo &quot;GRANT USAGE ON \$value.* TO &#039;\$dbusername&#039;@&#039;%&#039; REQUIRE NONE WITH MAX_QUERIES_PER_HOUR 0 MAX_CONNECTIONS_PER_HOUR 0 MAX_UPDATES_PER_HOUR 0 MAX_USER_CONNECTIONS 0; GRANT ALL PRIVILEGES ON `\$value`.* TO &#039;\$dbusername&#039;@&#039;%&#039;; GRANT ALL PRIVILEGES ON `\$value\_%`.* TO &#039;\$dbusername&#039;@&#039;%&#039;; \\r\\n&quot;;<br />
- //Example:<br />
- //GRANT USAGE ON robdba3.* TO &#039;robdba5&#039;@&#039;%&#039; REQUIRE NONE WITH MAX_QUERIES_PER_HOUR 0 MAX_CONNECTIONS_PER_HOUR 0 MAX_UPDATES_PER_HOUR 0 MAX_USER_CONNECTIONS 0; GRANT ALL PRIVILEGES ON `robdba3`.* TO &#039;robdba5&#039;@&#039;%&#039;; GRANT ALL PRIVILEGES ON `robdba3\_%`.* TO &#039;robdba5&#039;@&#039;%&#039;;<br />
- }<br />
+function myfunction($value,$key)<br />
+{<br />
+$dbusername = &quot;XXXXXXXXXX&quot;;<br />
+$ThePassword = generateRandomString(8); //Random 8 characters.<br />
+//echo &quot;The key $key has the value $value. &quot;;<br />
+echo &quot;CREATE USER &#039;$dbusername&#039;@&#039;%&#039;; IDENTIFIED VIA mysql_native_password USING &#039;$ThePassword&#039;; &lt;br /&gt;&quot;;<br />
+echo &quot;CREATE USER &#039;$dbusername&#039;@&#039;%&#039;; SET PASSWORD FOR &#039;$dbusername&#039;@&#039;%&#039; = PASSWORD(&#039;$ThePassword&#039;); &lt;br /&gt;&quot;;<br />
+echo &quot;GRANT USAGE ON $value.* TO &#039;$dbusername&#039;@&#039;%&#039; REQUIRE NONE WITH MAX_QUERIES_PER_HOUR 0 MAX_CONNECTIONS_PER_HOUR 0 MAX_UPDATES_PER_HOUR 0 MAX_USER_CONNECTIONS 0; &lt;br /&gt;&quot;;<br />
+echo &quot;GRANT ALL PRIVILEGES ON `$value`.* TO &#039;$dbusername&#039;@&#039;%&#039;; &lt;br /&gt;&quot;;<br />
+echo &quot;GRANT ALL PRIVILEGES ON `$value\_%`.* TO &#039;$dbusername&#039;@&#039;%&#039;; &lt;br /&gt;&lt;br /&gt;&quot;;<br />
+//Example:<br />
+//GRANT USAGE ON robdba3.* TO &#039;robdba5&#039;@&#039;%&#039; REQUIRE NONE WITH MAX_QUERIES_PER_HOUR 0 MAX_CONNECTIONS_PER_HOUR 0 MAX_UPDATES_PER_HOUR 0 MAX_USER_CONNECTIONS 0; GRANT ALL PRIVILEGES ON `robdba3`.* TO &#039;robdba5&#039;@&#039;%&#039;; GRANT ALL PRIVILEGES ON `robdba3\_%`.* TO &#039;robdba5&#039;@&#039;%&#039;;<br />
+}<br />
 //Remove these critical system databases from the output.<br />
- //information_schema<br />
- //mysql<br />
- //performance_schema<br />
- //sys<br />
+//information_schema<br />
+//mysql<br />
+//performance_schema<br />
+//sys<br />
 <br />
- \$i=count(\$rows);<br />
- for(\$x = 0; \$x &lt; \$i; \$x++){<br />
- 	\$y=\$rows[\$x]; //Reduce the array.<br />
- 	//array_diff to remove critical databases from the output.<br />
- 	//This prevents accidentally assigning permissions to critical system databases.<br />
- 	\$y = array_diff(\$y, array(&quot;information_schema&quot;, &quot;performance_schema&quot;,&quot;mysql&quot;,&quot;sys&quot;));<br />
- array_walk(\$y,&quot;myfunction&quot;);<br />
- }<br />
+$i=count($rows);<br />
+for($x = 0; $x &lt; $i; $x++){<br />
+$y=$rows[$x]; //Reduce the array.<br />
+//array_diff to remove critical databases from the output.<br />
+//This prevents accidentally assigning permissions to critical system databases.<br />
+$y = array_diff($y, array(&quot;information_schema&quot;, &quot;performance_schema&quot;,&quot;mysql&quot;,&quot;sys&quot;));<br />
+array_walk($y,&quot;myfunction&quot;);<br />
+}<br />
 <br />
-
 ?&gt;<br />
-";
-?>
