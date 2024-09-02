1b356592b2c49a89f2b4325d28fe0e268346d398
Robert Holland <rob@robholland.com>
2024-09-02 13:58:54 -0700

    php_local_server.php

---
+++ examples/php_local_server.php
@@ -0,0 +1,7 @@
+<?php
+echo "Type these commands to setup a local PHP server from a specific directory or folder.<br />";
+echo "sudo php --server localhost:8080 --docroot <br />";
+echo "or <br />";
+echo "php -S localhost:8000 <br />";
+
+?>
