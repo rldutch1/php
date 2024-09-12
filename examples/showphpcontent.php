2ecffe87b2a56cbd561dfe54ac5df48019557930
Robert Holland <rob@robholland.com>
2024-09-12 12:33:40 -0700

    Modified showphpcontent.php so that it handles dollar signs a little better. Not perfect yet.

--- examples/showphpcontent.php
+++ examples/showphpcontent.php
@@ -1,7 +1,11 @@
 <?php
 echo "
 &lt;?php<br />
-echo nl2br(htmlentities(file_get_contents( &quot;PHPfiletodisplay.php&quot; ))); // get the contents, and echo it out.<br />
+\$dollarsign = chr(36);<br />
+//echo \$dollarsign . &quot;&lt;br /&gt;&quot;;<br />
+echo str_replace(&quot;\\$&quot;,$dollarsign,nl2br(htmlentities(file_get_contents( &quot;PHPFileNameToDisplay.php&quot; )))); // get the contents, and echo it out.<br />
+//echo base64_decode(file_get_contents( &quot;jquery.dataTables.min.js.b64&quot; )); // get the contents, and echo it out.<br />
+//echo base64_encode(file_get_contents( &quot;jquery.dataTables.min.js&quot; )); // get the contents, and echo it out.<br />
 ?&gt;<br />
 ";
 ?>
