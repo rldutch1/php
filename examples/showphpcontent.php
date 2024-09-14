c9271ca04a793baba6823de9945c25418fb9577b
Robert Holland <rob@robholland.com>
2024-09-14 14:42:02 -0700

    showphpcontent.php is now complete.

--- examples/showphpcontent.php
+++ examples/showphpcontent.php
@@ -1,11 +1,14 @@
 <?php
 echo "
 &lt;?php<br />
-\$dollarsign = chr(36);<br />
-//echo \$dollarsign . &quot;&lt;br /&gt;&quot;;<br />
-echo str_replace(&quot;\\$&quot;,$dollarsign,nl2br(htmlentities(file_get_contents( &quot;PHPFileNameToDisplay.php&quot; )))); // get the contents, and echo it out.<br />
+echo &quot;&lt;?php\r\necho \&quot;\r\n&quot;;<br />
+&#36;filename=nl2br(htmlentities(file_get_contents( &quot;xb.php&quot; )));<br />
+&#36;pattern = &#039;/\&#36;/i&#039;; //Find the dollar sign.<br />
+echo preg_replace(&#36;pattern, &#039;&amp;#36;&#039;, &#36;filename); //Replace the dollar sign with the html equivalent.<br />
+echo &quot;\&quot;;\r\n?&gt;&quot;;<br />
+<br />
+//Miscellaneous:<br />
 //echo base64_decode(file_get_contents( &quot;jquery.dataTables.min.js.b64&quot; )); // get the contents, and echo it out.<br />
 //echo base64_encode(file_get_contents( &quot;jquery.dataTables.min.js&quot; )); // get the contents, and echo it out.<br />
-?&gt;<br />
-";
+?&gt;";
 ?>
