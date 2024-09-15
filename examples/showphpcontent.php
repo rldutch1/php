f116ba33403d5aa632a3667f948e7229170a9dcf
Robert Holland <rob@robholland.com>
2024-09-15 10:42:28 -0700

    Minor change to showphpcontent.php.

--- examples/showphpcontent.php
+++ examples/showphpcontent.php
@@ -2,7 +2,7 @@
 echo "
 &lt;?php<br />
 echo &quot;&lt;?php\r\necho \&quot;\r\n&quot;;<br />
-&#36;filename=nl2br(htmlentities(file_get_contents( &quot;xb.php&quot; )));<br />
+&#36;filename=nl2br(htmlentities(file_get_contents( &quot;PHPFileNameToDisplay.php&quot; )));<br />
 &#36;pattern = &#039;/\&#36;/i&#039;; //Find the dollar sign.<br />
 echo preg_replace(&#36;pattern, &#039;&amp;#36;&#039;, &#36;filename); //Replace the dollar sign with the html equivalent.<br />
 echo &quot;\&quot;;\r\n?&gt;&quot;;<br />
